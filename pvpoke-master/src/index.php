a7770c1deb8b1e8b621ee01bc60ffa56feb3259b
charliecap <charlesclinesdesk@gmail.com>
2020-10-03 19:59:55 -0400

    .

---
+++ pvpoke-master/src/index.php
@@ -0,0 +1,59 @@
+<?php require_once 'header.php'; ?>
+
+<div class="section home white">
+
+	<p>Welcome to PvPoke.com! We're an open-source tool for simulating, ranking, and building teams for Pokemon GO PvP (player versus player) battles. Check out the links below to get started.</p>
+
+	<a href="<?php echo $WEB_ROOT; ?>battle/" class="button">
+		<h2 class="icon-battle">Battle</h2>
+		<p>Simulate a battle between two custom Pokemon.</p>
+	</a>
+	<a href="<?php echo $WEB_ROOT; ?>train/" class="button">
+		<h2 class="icon-train">Train</h2>
+		<p>Play real-time battle simulations against a CPU opponent.</p>
+	</a>
+	<a href="<?php echo $WEB_ROOT; ?>rankings/" class="button">
+		<h2 class="icon-rankings">Rankings</h2>
+		<p>Explore the rankings, movesets, and counters for the top Pokemon in each league.</p>
+	</a>
+	<a href="<?php echo $WEB_ROOT; ?>team-builder/" class="button">
+		<h2 class="icon-team">Team Builder</h2>
+		<p>Build your own team and see their type matchups and potential counters.</p>
+	</a>
+	<a href="<?php echo $WEB_ROOT; ?>contribute/" class="button">
+		<h2 class="icon-contribute">Contribute</h2>
+		<p>Check out the source code on Github or lend your support through Patreon.</p>
+	</a>
+
+	<!--Update section for updates-->
+	<h3>What's New</h3>
+
+	<h4>v1.17.0 (September 28, 2020)</h4>
+	<ul>
+		<li>Decision algorithm overhauled for simulations:</li>
+		<ul>
+			<li>Move usage is now based on fastest sequence to KO (was formerly based on move damage per energy).</li>
+			<li>This should significantly improve optimal play in default matchups.</li>
+			<li>Huge thanks to <a href="https://twitter.com/Brandon50619618" target="_blank">Brandon/BananaBread</a> for contributing the code for this update!</li>
+		</ul>
+	</ul>
+
+	<h3>Latest Article</h3>
+
+	<div class="article-item flex">
+		<div class="col-3">
+			<a href="<?php echo $WEB_ROOT; ?>articles/ultimate-guide-to-team-building/">
+				<img src="<?php echo $WEB_ROOT; ?>assets/articles/team-building-thumb.jpg" />
+			</a>
+		</div>
+		<div class="col-9">
+			<h4><a href="<?php echo $WEB_ROOT; ?>articles/ultimate-guide-to-team-building/">Ultimate Team Building Guide by PolymersUp</a></h4>
+			<div class="date">August 11, 2020</div>
+			<p>Learn the fundamentals of building a good team and how to use the PvPoke Team Builder to its full potential.</p>
+		</div>
+
+	</div>
+
+</div>
+
+<?php require_once 'footer.php'; ?>
---
+++ pvpoke-master/src/js/interface/RankerInterface.js
@@ -0,0 +1,127 @@
+// JavaScript Document
+
+var InterfaceMaster = (function () {
+    var instance;
+
+    function createInstance() {
+
+
+        var object = new interfaceObject();
+
+		function interfaceObject(){
+
+			var battle;
+			var ranker = RankerMaster.getInstance();
+			var pokeSelectors = [];
+			var animating = false;
+			var self = this;
+
+			this.init = function(){
+
+				var data = GameMaster.getInstance().data;
+
+				$(".league-select").on("change", selectLeague);
+				$(".cup-select").on("change", selectCup);
+				$(".format-select").on("change", selectFormat);
+				$(".simulate").on("click", startRanker);
+
+				battle = new Battle();
+
+				// Load initial overrides
+
+				$.getJSON( webRoot+"data/overrides/all/1500.json?v="+siteVersion, function( data ){
+					if(ranker.setMoveOverrides){
+						ranker.setMoveOverrides(1500, "all", data);
+						console.log("Ranking overrides loaded [" + data.length + "]");
+					}
+				});
+
+			};
+
+			// Event handler for changing the league select
+
+			function selectLeague(e){
+				var allowed = [1500, 2500, 10000];
+				var cp = parseInt($(".league-select option:selected").val());
+
+				if(allowed.indexOf(cp) > -1){
+					battle.setCP(cp);
+				}
+
+				loadOverrides();
+
+			}
+
+			// Event handler for changing the cup select
+
+			function selectCup(e){
+				var cup = $(".cup-select option:selected").val();
+				battle.setCup(cup);
+
+				loadOverrides();
+			}
+
+			// Event handler for changing the format category
+
+			function selectFormat(e){
+				var format = $(".format-select option:selected").val();
+				var cup = $(".format-select option:selected").attr("cup");
+
+				$(".cup-select option").hide();
+				$(".cup-select option[cat=\""+format+"\"]").show();
+				$(".cup-select option[cat=\""+format+"\"]").eq(0).prop("selected", "selected");
+
+				if(cup){
+					$(".cup-select option[value=\""+cup+"\"]").eq(0).prop("selected", "selected");
+				}
+
+				$(".cup-select").trigger("change");
+
+				if((format == "all")||(cup)){
+					$(".cup-select").hide();
+				} else{
+					$(".cup-select").show();
+				}
+
+				if(format == "custom"){
+					// Redirect to the custom rankings page
+					window.location.href = webRoot+'custom-rankings/';
+				}
+
+				loadOverrides();
+			}
+
+			// Load overrides for the currently selected league and cup
+
+			function loadOverrides(){
+
+				var file = webRoot+"data/overrides/"+battle.getCup().name+"/"+battle.getCP()+".json?v="+siteVersion;
+
+				$.getJSON( file, function( data ){
+					if(ranker.setMoveOverrides){
+						ranker.setMoveOverrides(battle.getCP(), battle.getCup().name, data);
+						console.log("Ranking overrides loaded [" + data.length + "]");
+					}
+				});
+
+			}
+
+			// Run simulation
+
+			function startRanker(){
+				ranker.rankLoop(battle.getCP(), battle.getCup());
+			}
+		};
+
+        return object;
+    }
+
+    return {
+        getInstance: function () {
+            if (!instance) {
+                instance = createInstance();
+            }
+            return instance;
+        }
+    };
+})();
